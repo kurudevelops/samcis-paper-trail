dfc872f81ab724a97b95018e9c99926533fcfc6f
Lester Aranca <2240766@slu.edu.ph>
2026-07-14 15:15:42 +0800

    test: seed test user

--- backend/seed.py
+++ backend/seed.py
@@ -1,6 +1,8 @@
 import uuid
 from app.core.database import SessionLocal, Base, engine
 from app.features.departments.models import Department
+from app.features.user_roles.models import User
+from app.features.user_roles.models import RoleEnum
 
 # Ensure tables are created just in case
 Base.metadata.create_all(bind=engine)
@@ -42,6 +44,42 @@ def seed_departments():
     finally:
         db.close()
 
+def seed_users():
+    db = SessionLocal()
+    try:
+        # Check if the database is already seeded to prevent duplicates
+        if db.query(User).count() > 0:
+            print("Users are already seeded. Skipping.")
+            return
+
+        # Test users
+        users = [
+            {"first_name": "Conrado", "last_name": "Chan", "email": "cchan@slu.edu.ph", "role": RoleEnum.FACULTY}
+        ]
+
+        # Add each department to the database session
+        for usr in users:
+            new_usr = User(
+                id=str(uuid.uuid4()),
+                first_name=usr["first_name"],
+                last_name=usr["last_name"],
+                email=usr["email"],
+                role=usr["role"]
+            )
+            db.add(new_usr)
+
+        # Commit the transaction to save them to the database
+        db.commit()
+        print("Successfully seeded test users into the database!")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        db.rollback()
+    finally:
+        db.close()
+
 if __name__ == "__main__":
     print("Starting database seeding process...")
     seed_departments()
+    seed_users()
+    # seed_documents()
